5c24937bf4deff92e76191be26ac171b01390167
Elias Oumghar <eoumghar@gmail.com>
2021-11-16 13:07:23 +0100

    added a msg on console

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -14,8 +14,8 @@ exports.signup = (req, res, next) => {
          user
             .save()
             .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+
             .catch((error) => res.status(400).json({ error }));
-         console.log("Compte créé");
       })
       .catch((error) => res.status(500).json({ message: "Erreur lors de la création de l'utilisateur !" }));
 };
@@ -25,12 +25,14 @@ exports.login = (req, res, next) => {
       .then((user) => {
          if (!user) {
             return res.status(401).json({ error: "Utilisateur non trouvé !" });
+            console.log("Utilisateur non trouvé");
          }
          bcrypt
             .compare(req.body.password, user.password)
             .then((valid) => {
                if (!valid) {
                   return res.status(401).json({ error: "Mot de passe incorrect !" });
+                  console.log("Mot de passe incorrect");
                }
                res.status(200).json({
                   userId: user._id,
